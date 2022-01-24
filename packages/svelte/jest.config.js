d2e1bbf4564889094b16b4c84c7098aef5964664
zapaz.eth <alain@kredeum.com>
2022-01-24 15:27:53 +0100

    add svelte app

---
+++ packages/svelte/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'svelte',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(.+\\.svelte$)': [
+      'svelte-jester',
+      {
+        preprocess: 'packages/svelte/svelte.config.cjs',
+      },
+    ],
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['svelte', 'ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/svelte',
+};
